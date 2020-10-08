13be47cafbc4a67edaf2ac601781344ed19f9cf6
Stacy <lundquist.stacy@gmail.com>
2020-10-07 21:36:53 -0700

    whomp. So close....

--- lib/slack_api_wrapper.rb
+++ lib/slack_api_wrapper.rb
@@ -1,10 +1,26 @@
 # slack_api_wrapper.rb
-require "httparty"
+require 'httparty'
 
 module SlackApiWrapper
-  BASE_URL = "https://slack.com/api/"
-  API_KEY = ENV["SLACK_TOKEN"]
+  BASE_URL = 'https://slack.com/api'
+  API_KEY = ENV['SLACK_TOKEN']
 
-  # Code goes here
+  def self.send_msg(message, channel)
+    response = HTTParty.post(
+      "#{BASE_URL}/chat.postMessage",
+      body: {
+        token: API_KEY,
+        text: message,
+        channel: channel
+      },
+      headers: { 'Content-Type' => 'application/x-www-form-urlencoded' }
+    )
+
+    if response.code != 200 || response["ok"] == false
+      raise StandardError, "there is a problem: #{response['error']}"
+    end
+    return response.code == 200 && response.parsed_response['ok']
+
+  end
 
 end
--- test/slack_api_wrapper_test.rb
+++ test/slack_api_wrapper_test.rb
@@ -2,5 +2,10 @@
 require_relative 'test_helper'
 
 describe SlackApiWrapper do
-
+  it "can send a valid message" do
+    VCR.use_cassette("slack-posts") do
+      response = SlackApiWrapper.send_msg("Posting with ruby!", "general")
+      expect(response).must_equal true
+    end
+  end
 end
